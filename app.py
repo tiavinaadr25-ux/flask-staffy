2bab5d0051f1269aeae031adfc3302ea9cd190e5
tiavinaadr25-ux <mialisoaadr25@gmail.com>
2026-03-13 12:32:43 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Identifiants fixes (pas de base de données pour l'instant)
+EMAIL = "manager@staffly.com"
+MOT_DE_PASSE = "staffly123"
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/connexion', methods=['POST'])
+def connexion():
+    email = request.form['email']
+    mdp = request.form['mdp']
+
+    if email == EMAIL and mdp == MOT_DE_PASSE:
+        return render_template('bravo.html')
+    else:
+        return render_template('login.html', erreur="Email ou mot de passe incorrect !")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
